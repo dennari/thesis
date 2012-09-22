c9e266139972c5cd7c2d954bdc0865ee03ae490b
Ville Väänänen <ville.vaananen@aalto.fi>
2012-09-22 09:48:30 +0300

    sfgsd

--- code/ball/BallisticCompute.m
+++ code/ball/BallisticCompute.m
@@ -3,15 +3,15 @@
 global dt m0 P0 H A g0x g0y u
 
 
-N = 700;
+N = 15000;
 T = 14;
 dt = T/N;
 
 
 %%%%%%%%%%%% PARAMETERS %%%%%%%%%%%%
-lqx = log(0.8);       % parameterization in logarithm of standard deviation
-lqy = lqx-log(3);
-lr =  log(3);
+lqx = log(0.2);       % parameterization in logarithm of standard deviation
+lqy = lqx-log(10);
+lr =  log(0.001);
 
 
 K = (0:N)*dt;
@@ -47,12 +47,12 @@ P0 = eye(size(m0,1));%diag([1e-6 7^-2 1e-6 7^-2]);
 % 2=lqy,  y process log(std)
 % 3=lr,   measurement log(std)
 pNames = {'qx' 'qy' 'r'};
-gis = eye(3);
-fn = '../data/Ballistic_%s_%.0f';
-iters = [5 20;
+gis = [1 1 0];
+fn = '../data/Ballistic_%s_%.0f_%.0f';
+iters = [10 10;
          5  10;
          5 20;];
-NNs = [100 100 100];
+NNs = [10 100 100];
 % iters = [10 10;
 %          10  10;
 %          10 10;];
@@ -128,7 +128,7 @@ end
 
 %plot(max_iter_em,est_em')
 
-save(sprintf(fn,pNames{i},NN));
+save(sprintf(fn,pNames{gi},NN,N));
 
 end
 
