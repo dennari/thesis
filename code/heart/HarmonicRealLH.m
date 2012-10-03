ce69d439a3d2b18d69f72a0a9a0c0f272c90e0d4
Ville Väänänen <ville.vaananen@aalto.fi>
2012-10-04 01:00:54 +0300

    evaluate LH on a grid

--- code/heart/HarmonicRealLH.m
+++ code/heart/HarmonicRealLH.m
@@ -1,70 +1,45 @@
 
 %% Setup
-global dt c
-S = load('../data/dataa_villelle.mat','card_data','data_t');
-y = S.card_data;
-K = S.data_t;
-clear S;
-offset = 20;
-secs = 57;
-%secs = 1;
-starti = find(K<offset,1,'last'); 
-endi = find(K<offset+secs,1,'last');
-y = y(starti:endi);
-K = K(starti:endi);
-K = K-K(1);
-
-
-ds = round(40*secs/60);
-%ds = 15;
-% downsample by ds
-y = y(1:ds:end)';
-K = K(1:ds:end);
-
-dt = K(2)-K(1);
-N = length(K);
-T = K(end);
-
-
-% the parameters of this model
-r = 0.0001;          % log measurement noise
-Nqx = 100;
-Nqw = 100;
-qx_range = linspace(0.04,0.08,Nqx);
-qw_range = linspace(0.2,0.35,Nqw);
+load('../data/Harmonic_lqw_lqx_10_3751.mat');
+Nqx = 50;
+Nqw = 50;
+qx_range = linspace(exp(-5.5),exp(-1.5),Nqx);
+qw_range = linspace(exp(-2.2),exp(-0.05),Nqw);
 
 
 
 [QX,QW] = meshgrid(qx_range,qw_range);
+%break
 
-
-c = 3; % number of harmonics (including the fundamental frequency)
-xDim = 2*c+1;
-H = [0 repmat([1 0],1,c)];
-h = @(x) H*x;
-Jh = @(x) H;
-f = @(x) sinusoid_f(x);
-Jf = @(x) sinusoid_Jf(x);
-m0 = [0.3 zeros(1,xDim-1)]';
-P0 = eye(xDim);
-
-
-% adjust K and y to include the zeroth measurement
-K = [0 K+dt];
-Y = [H*m0 y];
-
+% c = 3; % number of harmonics (including the fundamental frequency)
+% xDim = 2*c+1;
+% H = [0 repmat([1 0],1,c)];
+% h = @(x) H*x;
+% f = @(x) sinusoid_f(x);
+% m0 = [0.3 zeros(1,xDim-1)]';
+% P0 = eye(xDim);
+% 
+% 
+% % adjust K and y to include the zeroth measurement
+% K = [0 K+dt];
+% Y = [H*m0 y];
+if matlabpool('size') == 0 % checking to see if my pool is already open
+    matlabpool open 20
+end
 
 fn = '../data/HarmonicLH_%.0f_%.0f';
 
-SR = sqrt(r);
+SR = sqrt(sinusoid_R(lr));
 [usig,w] = CKFPoints(xDim);
 w(:,2) = sqrt(w(:,1)); % add weights for square root filt/smooth
-
+Qf = @(qw,qx) diag([qw^2, 0, qx^2, 0, qx^2, 0, qx^2]);
+f = @(x) sinusoid_f(x,dt);
 Z = zeros(Nqw,Nqx);
 %tic
-for i=1:Nqx
+parfor i=1:Nqx
+  tmp = zeros(Nqw,1);
   for j=1:Nqw   
-    SQ = sqrt(sinusoid_Q(QW(j,i),QX(j,i),1));
+    SQ = sqrt(Qf(QW(j,i),QX(j,i)));
     lh = 0; m = m0; S = P0;
     for k=1:(N+1)
         [m_,S_] = SigmaKF_Predict(m,S,f,SQ,usig,w);
@@ -79,11 +54,13 @@ for i=1:Nqx
 
 
     end
-    Z(j,i) = lh;
-    fprintf('%.2f\n',100*((i-1)*Nqw+j)/(Nqx*Nqw));
+    tmp(j) = lh;
+    %fprintf('%.2f\n',100*((i-1)*Nqw+j)/(Nqx*Nqw));
 
   
   end
+  i
+  Z(:,i) = tmp;
 end
 %tm = toc
 save(sprintf(fn,Nqx,Nqw));
