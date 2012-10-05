bea5d90fce4965c07f1bd0177b1d03dd2d5f1192
Ville Väänänen <ville.vaananen@aalto.fi>
2012-10-05 15:59:20 +0300

    edfgd

--- code/heart/HarmonicRealInnerLH.m
+++ code/heart/HarmonicRealInnerLH.m
@@ -1,32 +1,12 @@
-function tmp=HarmonicRealInnerLH(QW,QX,Nqw,H,Y,SR,usig,w,m0,P0,dt)
-%'fghfghf'
-%pwd()
-Qf = @(qw,qx) diag([qw^2, 0, qx^2, 0, qx^2, 0, qx^2]);
-   f = @(x) sinusoid_f(x,dt);
-   h = @(x) H*x;
+function tmp=HarmonicRealInnerLH(QW,QX,Nqw)
+%global dt c m0 P0 h f Jh Jf
+load('../data/Harmonic_lqw_lqx_10_3751.mat');
 tmp = zeros(Nqw,1);
-  N = size(Y,2)-1;
-  for j=1:Nqw   
-    SQ = sqrt(Qf(QW(j),QX(j)));
-    lh = 0; m = m0; S = P0;
-    
-    for k=1:(N+1)
-        [m_,S_] = SigmaKF_Predict(m,S,f,SQ,usig,w);
-
-        if k==N+1; break; end; 
-
-      
-        [m,S,~,my,CSy] = SigmaKF_Update(m_,S_,Y(:,k+1),h,SR,usig,w);
-        %%% CSy and CC are Cholesky decompositions _HERE_ %%%
-        Sy = CSy*CSy';
-        lh = lh + likelihood(Y(:,k+1)-my,Sy);
-
-
-    end
-    j
-    tmp(j) = lh;
-    %fprintf('%.2f\n',100*((i-1)*Nqw+j)/(Nqx*Nqw));
-  end
+p = p0;
+for j=1:Nqw   
+    p(gi) = [QW(j) QX(j)];
+    tmp(j) = Harmonic_LH(p,Y);
+end
 
 
 
--- code/heart/HarmonicRealLH.m
+++ code/heart/HarmonicRealLH.m
@@ -2,8 +2,8 @@
 load('../data/Harmonic_lqw_lqx_10_3751.mat');
 Nqx = 50;
 Nqw = 50;
-qx_range = linspace(exp(-5.5),exp(-1.5),Nqx);
-qw_range = linspace(exp(-2.2),exp(-0.05),Nqw);
+qx_range = linspace(-5.5,-1.5,Nqx);
+qw_range = linspace(-2.2,-0.05,Nqw);
 
 [QX,QW] = meshgrid(qx_range,qw_range);
 
@@ -11,15 +11,12 @@ qw_range = linspace(exp(-2.2),exp(-0.05),Nqw);
 
 fn = '../data/HarmonicLH_%.0f_%.0f';
 
-SR = sqrt(sinusoid_R(lr));
-[usig,w] = CKFPoints(xDim);
-w(:,2) = sqrt(w(:,1)); % add weights for square root filt/smooth
-
 Z = zeros(Nqw,Nqx);
 %tic
 jobs = cell(Nqx,1);
 for i=1:Nqx
-  jobs{i} = batch(@HarmonicRealInnerLH,1,{QW(:,i),QX(:,i),Nqw,H,Y,SR,usig,w,m0,P0,dt},'PathDependencies','./heart');
+  %jobs{i} = batch(@HarmonicRealInnerLH,1,{QW(:,i),QX(:,i),Nqw,H,Y,SR,usig,w,m0,P0,dt},'PathDependencies','./heart');
+  Z(i) = HarmonicRealInnerLH(QW(:,i),QX(:,i),Nqw);
   %wait(j);
   %diary(j)
   %tmp = getAllOutputArguments(j)
@@ -34,8 +31,8 @@ for i=1:Nqx
 end
 
 %%
-
-surf(QX,QW,Z); hold on;
+figure(1); clf;
+surf(log(QX),log(QW),Z); hold on;
 %plot3([exp(lqx) exp(lqx)],[exp(lqw) exp(lqw)],[min(min(Z)) max(max(Z))]);
 xlabel('qx');
 ylabel('qw');
